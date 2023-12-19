d3875c10fdc5507bcc01213738006e1f58dce5d7
NelsonZaraza <nelsonmauricionavarrozaraza@gmail.com>
2023-12-19 17:15:04 -0500

    Proyecto front-end super Mario Bros terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
 
       backgroundImage:{
         'wave-pattern': 'url(../img/wave-white.png)',
-        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)'
+        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
+        'pattern-characters-red': 'url(../img/pattern-characters-red.png)',
+        'character-mario': 'url(../img/character-l.png)',
+        'wave-pink':'url(../img/wave-pink.png)',
+        'yellow-dots':'url(../img/pattern-yellow-dots.png)'
       },
       screens:{
         'xsm': '320px'
